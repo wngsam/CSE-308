12dc6a42b964f58982dd6e083d58d5ba80f153b0
Sam Wang <wngsam@gmail.com>
2015-04-09 22:46:03 -0400

    Added TheaterManager

---
+++ src/java/managers/TheaterManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managers;
+
+import domains.Theater;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Sam W.
+ */
+public class TheaterManager {
+    
+    HashMap<String,Theater> theaters;
+    
+    public TheaterManager(){
+        theaters = new HashMap();
+    }
+    
+}
